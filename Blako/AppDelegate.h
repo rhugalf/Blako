a9650899001b4a3c30ccaee12c9957435cb0c85f
A webo putos <glb-mxm0004@MacBook-Pro-de-GLB-MXM0004.local>
2015-07-13 12:54:40 -0500

    Initial Commit

---
+++ Blako/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blako
+//
+//  Created by A webo putos on 13/07/15.
+//  Copyright (c) 2015 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blako/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blako
+//
+//  Created by A webo putos on 13/07/15.
+//  Copyright (c) 2015 Hugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
